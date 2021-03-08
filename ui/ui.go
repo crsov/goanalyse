532a7eeb4bc88d208d5f56c8375db4571aea71ab
crsov <burtsev.kolya@yandex.ru>
2021-03-08 19:06:02 +0300

    adding this repo to github

---
+++ ui/ui.go
@@ -0,0 +1,41 @@
+package ui
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+func StartUi() {
+	ui, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer ui.Close()
+
+	ui.SetManagerFunc(layout)
+
+	if err := ui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := ui.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(ui *gocui.Gui) error {
+	maxX, maxY := ui.Size()
+	if v, err := ui.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(ui *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
