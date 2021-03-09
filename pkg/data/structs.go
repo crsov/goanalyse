686032f108d52abe9abb46b7eee1eba284c987a2
crsov <burtsev.kolya@yandex.ru>
2021-03-09 22:23:04 +0300

    Adding xml parsing

--- pkg/data/structs.go
+++ pkg/data/structs.go
@@ -1,43 +1,35 @@
 package data
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+)
+
+type roRoot struct {
+	XMLName xml.Name `xml:"root"`
+	root    []roGame
+}
 
 type roGame struct {
-	XMLName xml.Name `xml:"game"`
-	id      int      `xml:"id"`
-	black   string   `xml:"black"`
-	white   string   `xml:"white"`
-	winner  string   `xml:"winner"`
-	moves   string   `xml:"moves"`
-	rule    string   `xml:"rule"`
-}
-
-type rnGame struct {
-}
-
-type rnPlayer struct {
-	id      int
-	name    string
-	country int
-}
-
-type rnRule struct {
-	id   int
-	name string
-}
-
-type rnCity struct {
-	id      int
-	country int
-}
-
-type rnOpenning struct {
-	id   int
-	abbr string
-}
-
-type rnCountry struct {
-	id   int
-	name string
-	abbr string
+	XMLName         xml.Name `xml:"game"`
+	id              int      `xml:"id"`
+	creation_time   int      `xml:"creation_time"`
+	black           string   `xml:"black"`
+	white           string   `xml:"white"`
+	turn_color      string   `xml:"turn_color"`
+	board           string   `xml:"board"`
+	moves           string   `xml:"moves"`
+	alt5            string   `xml:"alt_5"`
+	proposition     string   `xml:"proposition"`
+	proposer        string   `xml:"proposer"`
+	status          string   `xml:"status"`
+	rule            string   `xml:"rule"`
+	time            int      `xml:"time"`
+	tpm             int      `xml:"tpm"`
+	start_time      int      `xml:"start_time"`
+	end_time        int      `xml:"end_time"`
+	winner          string   `xml:"winner"`
+	winby           string   `xml:"winby"`
+	time_left_black int      `xml:"time_left_black"`
+	time_left_white int      `xml:"time_left_white"`
+	tid             string   `xml:"tid"`
 }
