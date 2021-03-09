dae6b5c225e1eb63f01190e65bcb9ad70eec693c
crsov <burtsev.kolya@yandex.ru>
2021-03-09 19:37:37 +0300

    Adding xml parsing

---
+++ cmd/goanalyse/goanalyse.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/crsov/goanalyse/pkg/cui"
+	"github.com/crsov/goanalyse/pkg/data"
+)
+
+func main() {
+	data.Xml()
+	cui.StartUi()
+}
---
+++ pkg/cui/cui.go
@@ -0,0 +1,28 @@
+package cui
+
+import (
+	"github.com/jroimartin/gocui"
+	"log"
+)
+
+func StartUi() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
