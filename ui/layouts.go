21ab03e74d0a5777a6b8f73ff1cee55d271d5a3f
crsov <burtsev.kolya@yandex.ru>
2021-03-09 11:14:58 +0300

    Re-testings...

--- ui/layouts.go
+++ ui/layouts.go
@@ -8,7 +8,7 @@ import (
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2-2, maxX/2+2, maxY/2+7); err != nil {
+	if v, err := g.SetView("hello", 0, maxY, maxX, 0); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
