3c40ca4922ff79153bf3794d004dd524269c957b
crsov <burtsev.kolya@yandex.ru>
2021-03-09 12:57:26 +0300

    Adding main cui elements

--- cui/layout.go
+++ cui/layout.go
@@ -8,17 +8,21 @@ import (
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if board_view, err := g.SetView("board", 0, 0, int(0.6*float32(maxX)), maxY-1); err != nil {
+	board_view, err := g.SetView("board", 0, 0, int(0.6*float32(maxX)), maxY-1)
+	if err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(board_view, maxX, maxY)
 	}
-	if games_view, err := g.SetView("games", int(0.6*float32(maxX)), 0, int(0.4*float32(maxX)), maxY-1); err != nil {
+	games_view, err := g.SetView("games", int(0.6*float32(maxX)), 0, int(0.4*float32(maxX)), maxY-1)
+	if err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(games_view, maxX, maxY)
 	}
+
+	fmt.Fprintln(board_view, maxX, maxY)
+	fmt.Fprintln(games_view, maxX, maxY)
+
 	return nil
 }
