ad198019206d77322e2205925da1c62bcbd06698
crsov <burtsev.kolya@yandex.ru>
2021-03-09 21:40:40 +0300

    Adding xml parsing

--- pkg/data/xml.go
+++ pkg/data/xml.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -16,14 +17,12 @@ func Xml() {
 
 	defer xmlFile.Close()
 
-	//decoder := charmap.Windows1252.NewDecoder()
-	//reader := decoder.Reader(xmlFile)
 	inBytes, err := ioutil.ReadAll(xmlFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(inBytes))
-	// var roGames roGame
-	// xml.Unmarshal(inBytes, &roGames)
-	// fmt.Println(roGames)
+	var roGames roGame
+	xml.Unmarshal(inBytes, &roGames)
+	fmt.Println(roGames.white)
+
 }
