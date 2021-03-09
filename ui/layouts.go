d7cc370d28347d7d5e2dc04d6234fd6b5e88b6ca
crsov <burtsev.kolya@yandex.ru>
2021-03-09 11:07:25 +0300

    Re-test

--- ui/layouts.go
+++ ui/layouts.go
@@ -5,14 +5,13 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
-func layout(ui *gocui.Gui) error {
-	maxX, maxY := ui.Size()
-
-	if view, err := ui.SetView("hello", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(view, "fskfjsdhflkjh")
+		fmt.Fprintln(v, "Hello world!")
 	}
 	return nil
 }
--- ui/ui.go
+++ ui/ui.go
@@ -1,30 +1,28 @@
 package ui
 
 import (
-	"log"
-
 	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func StartUi() {
-	ui, err := gocui.NewGui(gocui.OutputNormal)
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
 	}
+	defer g.Close()
 
-	defer ui.Close()
+	g.SetManagerFunc(layout)
 
-	ui.SetManagerFunc(layout)
-
-	if err := ui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
 
-	if err := ui.MainLoop(); err != nil && err != gocui.ErrQuit {
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 }
 
-func quit(ui *gocui.Gui, view *gocui.View) error {
+func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
