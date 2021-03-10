ff2ebe8337352231056519983413338c12434c5e
crsov <burtsev.kolya@yandex.ru>
2021-03-10 14:24:52 +0300

    Adding xml parsing

--- pkg/data/structs.go
+++ pkg/data/structs.go
@@ -1,17 +1,14 @@
 package data
 
-import "encoding/xml"
-
 type roRoot struct {
-	XMLNme  xml.Name `xml:"root"`
 	RoGames []roGame
 }
 type roGame struct {
-	Id     int    `xml:"id"`
-	Black  string `xml:"black"`
-	White  string `xml:"white"`
-	Moves  string `xml:"moves"`
-	Alt5   string `xml:"alt_5"`
-	Rule   string `xml:"rule"`
-	Winner string `xml:"winner"`
+	Id     int    `xml:"game>id"`
+	Black  string `xml:"game>black"`
+	White  string `xml:"game>white"`
+	Moves  string `xml:"game>moves"`
+	Alt5   string `xml:"game>alt_5"`
+	Rule   string `xml:"game>rule"`
+	Winner string `xml:"game>winner"`
 }
