6d14755b4e50310de866ddc5faec1e9ff5cf8c9e
crsov <burtsev.kolya@yandex.ru>
2021-03-15 14:22:11 +0300

    ...

---
+++ pkg/data/convert.go
@@ -0,0 +1,12 @@
+package data
+
+import "fmt"
+
+
+func ConvertRo(ro ro) mainf{
+//input {[games]}
+	fmt.Println(ro.RoGames[0])
+	return mainf{}
+}
+
+
---
+++ pkg/gui/gui.go
@@ -0,0 +1,12 @@
+package gui
+
+import (
+	"gioui.org/ui/app"
+)
+
+func StartUi()  {
+	win := app.
+	for event in range win.Events() {
+
+	}
+}
