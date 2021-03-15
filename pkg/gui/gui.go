6503cdbcb20edc2591b707d05564f41cdbacbee3
crsov <burtsev.kolya@yandex.ru>
2021-03-15 14:39:28 +0300

    ///

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -1,9 +1,15 @@
 package gui
 
 import (
+	"github.com/webview/webview"
+)
 
-	)
-
-func StartUi()  {
-
+func StartUi() {
+	debug := true
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetTitle("Minimal webview example")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+	w.Run()
 }
