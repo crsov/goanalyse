c86211edeb655618d9c9b68b8073dd89068931b2
crsov <burtsev.kolya@yandex.ru>
2021-03-16 13:02:54 +0300

    Refactor added html && js

--- js.js
+++
@@ -1,9 +0,0 @@
-canv = Document.querySelector("#canvas")
-let ctx = canv.getContext('2d')
-
-let boardImage = new Image()
-boardImage.src = 'board.png'
-
-window.onload = () => {
-   ctx.drawImage(boardImage, 12, 12)
-}
