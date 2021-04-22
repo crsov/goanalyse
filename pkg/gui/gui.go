0a010b3104dd2397f2ce975fc1f5769c759f4e1d
crsov <aruthecrsov@ya.ru>
2021-04-22 21:11:44 +0300

    Update...

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -8,34 +8,15 @@ import (
 func StartUi() {
 	gtk.Init(nil)
 
-	// Создаём окно верхнего уровня, устанавливаем заголовок
-	// И соединяем с сигналом "destroy" чтобы можно было закрыть
-	// приложение при закрытии окна
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	// Создаём билдер
+	blder, err := gtk.BuilderNew()
 	if err != nil {
-		log.Fatal("Не удалось создать окно:", err)
-	}
-	win.SetTitle("Простой пример")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
-
-	// Создаём новую метку чтобы показать её в окне
-	l, err := gtk.LabelNew("Привет, gotk3!")
-	if err != nil {
-		log.Fatal("Не удалось создать метку:", err)
+		log.Fatal("Ошибка:", err)
 	}
 
-	// Добавляем метку в окно
-	win.Add(l)
-
-	// Устанавливаем размер окна по умолчанию
-	win.SetDefaultSize(800, 600)
-
-	// Отображаем все виджеты в окне
-	win.ShowAll()
-
-	// Выполняем главный цикл GTK (для отрисовки). Он остановится когда
-	// выполнится gtk.MainQuit()
-	gtk.Main()
+	// Загружаем в билдер окно из файла Glade
+	err = blder.AddFromFile("goanalyse.glade")
+	if err != nil {
+		log.Fatal("Ошибка:", err)
+	}
 }
