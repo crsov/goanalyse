50c19ddf6bae5f413e92b71d375fea9a983a207f
crsov <aruthecrsov@ya.ru>
2021-04-22 21:20:17 +0300

    Update...

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -19,4 +19,20 @@ func StartUi() {
 	if err != nil {
 		log.Fatal("Ошибка:", err)
 	}
+
+	// Получаем объект главного окна по ID
+	obj, err := blder.GetObject("window_main")
+	if err != nil {
+		log.Fatal("Ошибка:", err)
+	}
+
+	// Преобразуем из объекта именно окно типа gtk.Window
+	// и соединяем с сигналом "destroy" чтобы можно было закрыть
+	// приложение при закрытии окна
+	win := obj.(*gtk.Window)
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	win.showAll()
+	gtk.main()
 }
